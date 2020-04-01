265cdf38691b207a798bbeb5b13465d8898e5b1c
joseiltonsilva <sjoseilton@gmail.com>
2020-03-31 23:39:58 -0300

    Back-end e front-end da aplicação

---
+++ backend/src/routes.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const OngController = require('./controllers/OngController');
+const IncidentController = require('./controllers/IncidentController');
+const ProfileController = require('./controllers/ProfileController');
+const SessionControler = require('./controllers/SessionController');
+
+const routes = express.Router();
+
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
+
+routes.get('/profile', ProfileController.index);
+
+routes.post('/session', SessionControler.create);
+
+
+// serve para expor uma rota
+module.exports = routes;
